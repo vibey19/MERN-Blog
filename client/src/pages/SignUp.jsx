e5f48213aa50e27d00858c0a5eed547a32628847
vibey19 <jdadhaniya6@gmail.com>
2024-02-17 12:00:39 +0530

    complete sign-up page UI

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,55 @@
+import { Button, Label, TextInput } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-9">
+        {/*Left*/}
+        <div className="flex-1">
+          <Link to="/" className=" text-4xl font-bold dark:text-white">
+            <span className="px-2 py-1 rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
+              Nevermore
+            </span>
+            Notes
+          </Link>
+          <p className="text-sm mt-5">
+            Liberate Your Voice. Immerse yourself in a realm where your words
+            wield raw power. Begin typing, commence conquering.
+          </p>
+        </div>
+        {/*Right*/}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your Username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your Email" />
+              <TextInput
+                type="text"
+                placeholder="name@company.com"
+                id="email"
+              />
+            </div>
+            <div>
+              <Label value="Your Password" />
+              <TextInput type="text" placeholder="Password" id="password" />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-3 text-sm mt-5 font-semibold">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Log In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
