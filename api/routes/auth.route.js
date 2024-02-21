b4f803af762064b62cde54590bd19a2044cf788d
vibey19 <jdadhaniya6@gmail.com>
2024-02-21 13:44:11 +0530

    complete dashboard profile UI

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,13 +1,11 @@
-import express from "express";
-import { signup } from '../controllers/auth.controller.js'
-import { signin } from "../controllers/auth.controller.js"
-import { google } from "../controllers/auth.controller.js";
+import express from 'express';
+import { google, signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
+
 
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.post('/google', google)
 
-
-export default router
+export default router;
