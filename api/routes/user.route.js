9bbc9d15b4d07c80c0d7507038c89823c413daa6
vibey19 <jdadhaniya6@gmail.com>
2024-02-21 14:40:15 +0530

    update user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,5 +4,6 @@ import { test } from "../controllers/user.controller.js";
 const router = express.Router();
 
 router.get('/test', test);
+router.put('/update/:userId', verifyToken, updateUser);
 
 export default router;
