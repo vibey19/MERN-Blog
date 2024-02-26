9a3ade14d94cc32886c319518e022cf9158102c9
vibey19 <jdadhaniya6@gmail.com>
2024-02-26 18:45:54 +0530

    create add post functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FooterComp from "./assets/components/FooterComp";
 import PrivateRoute from "./assets/components/PrivateRoute";
 
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import CreatePost from "./pages/CreatePost";
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Header></Header>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -21,6 +24,10 @@ export default function App() {
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path="/create-post" element={<CreatePost />} />
+        </Route>
+
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterComp />
