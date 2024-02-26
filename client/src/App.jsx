5bf3b969b0d05b7f264ac77d6b964031782d110b
vibey19 <jdadhaniya6@gmail.com>
2024-02-26 18:48:30 +0530

    fixed import bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,6 @@ import Dashboard from "./pages/Dashboard";
 import Header from "./assets/components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FooterComp from "./assets/components/FooterComp";
-import PrivateRoute from "./assets/components/PrivateRoute";
-
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 
